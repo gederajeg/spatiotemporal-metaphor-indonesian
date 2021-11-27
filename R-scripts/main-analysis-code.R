4e878917448a86d7ce19ba67f0d46f709e33d3fb
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2021-11-27 13:49:37 +0800

    Add codes for the analysis of the spatial words; more to come.

--- R-scripts/main-analysis-code.R
+++ R-scripts/main-analysis-code.R
@@ -219,6 +219,134 @@ abline(h = 1/2, col = "red", lty = 2)
 text(x = 1.9, y = 0.53, labels = "Expected proportion", col = "red")
 dev.off()
 
+## DEPAN, BELAKANG, DATANG, MENDATANG, LALU ========
+namahari <- c("senin", "selasa",
+              "rabu", "kamis",
+              "jumat", "sabtu",
+              "minggu", "pekan")
+
+namabulan <- c("januari", "january", "februari", "february",
+               "maret", "march", "april", "mei", "may", "juni", "juli", "july", "august",
+               "agustus", "september", "oktober", "october", "november",
+               "desember", "december")
+
+timenouns <- c(namahari, namabulan, "tahun", "bulan", "musim", "masa", "waktu")
+
+timeunits <- c("tahun", "bulan", "hari", "era", "jaman", "zaman", "masa", "waktu", "saat", "jam", "menit", "detik", "sepekan", "pekan", "minggu", "periode", "period", "momen")
+
+timewords_all <- unique(c(namahari, namabulan, timenouns, timeunits))
+
+peekgram <- function(ngram = NULL, pattern = NULL) {
+  tbl <- filter(ngram, if_any(starts_with("w"), ~str_detect(., pattern)))
+  return(tbl)
+}
+
+# gr2 <- read_tsv("data_ngram_raw/ngram_2_raw.txt") # read the whole-corpus 2-gram data (387MB!)
+# gr3 <- read_tsv("data_ngram_raw/ngram_3_raw.txt") # read the whole-corpus 3-gram data (540.2MB!)
+# gr4 <- read_tsv("data_ngram_raw/ngram_4_raw.txt") # read the whole-corpus 4-gram data (666.5MB!)
+# timewords_depan <- gr2 %>% filter(w1 %in% timewords_all, w2 == "depan")
+# timewords_belakang <- gr2 %>% filter(w1 %in% timewords_all, w2 == "belakang")
+# write_tsv(timewords_depan, file = "data/timewords_depan.txt")
+# write_tsv(timewords_belakang, file = "data/timewords_belakang.txt")
+timewords_depan <- read_tsv("data/timewords_depan.txt")
+timewords_belakang <- read_tsv("data/timewords_belakang.txt")
+
+# all 2-, 3-, 4-, and 5-gram DEPAN data
+(depan_2gr <- read_tsv("data/depan_2grams.txt"))
+(depan_3gr <- read_tsv("data/depan_3grams.txt"))
+(depan_4gr <- read_tsv("data/depan_4grams.txt"))
+(depan_5gr <- read_tsv("data/depan_5grams.txt"))
+
+## Analysis on [TIME NOUN + depan]
+timewords_depan %>% count(ngram, sort = TRUE) # masa depan 'period front; future' is the most frequent, followed by tahun depan 'year front; next year'
+## check the full sentences
+# mycorpus_filepath <- "/Users/Primahadi/Documents/Corpora/_corpusindo/Leipzig Corpora/ind_newscrawl_2016_1M-sentences.txt"
+# corplingr::concord_leipzig(mycorpus_filepath, pattern = "\\bhari\\sdepan\\b") %>% pull(node_sentences) %>% write_lines(file = "data/sent_hari_depan.txt")
+(sent_hari_depan <- read_lines("data/sent_hari_depan.txt"))
+
+# all 2-, 3-, 4-, and 5-gram BELAKANG data
+(belakang_2gr <- read_tsv("data/belakang_2grams.txt"))
+(belakang_3gr <- read_tsv("data/belakang_3grams.txt"))
+(belakang_4gr <- read_tsv("data/belakang_4grams.txt"))
+(belakang_5gr <- read_tsv("data/belakang_5grams.txt"))
+
+## Analysis on [TIME NOUN + belakang]
+timewords_belakang %>% count(ngram, sort = TRUE) # very low frequency (only three types [five tokens])
+## check the full sentences
+# mycorpus_filepath <- "/Users/Primahadi/Documents/Corpora/_corpusindo/Leipzig Corpora/ind_newscrawl_2016_1M-sentences.txt"
+# corplingr::concord_leipzig(mycorpus_filepath, pattern = "\\b(tahun|bulan|sepekan)\\sbelakang\\b") %>% pull(node_sentences) %>% write_lines(file = "data/sent_TIME_belakang.txt")
+(sent_TIME_belakang <- read_lines("data/sent_TIME_belakang.txt"))
+
+## checking 'akan mendatang' in 2-, 3-, and 4-gram data returns zero results (see below)
+# > gr2 %>% filter(w2 == "mendatang", w1 == "akan")
+# # A tibble: 0 x 3
+# # … with 3 variables: ngram <chr>, w1 <chr>, w2 <chr>
+# > gr3 %>% filter(w3 == "mendatang", w2 == "akan")
+# # A tibble: 0 x 4
+# # … with 4 variables: ngram <chr>, w1 <chr>, w2 <chr>, w3 <chr>
+# > gr4 %>% filter(w4 == "mendatang", w3 == "akan")
+# # A tibble: 0 x 5
+# # … with 5 variables: ngram <chr>, w1 <chr>, w2 <chr>, w3 <chr>, w4 <chr>
+
+## checking 'yang mendatang' produces two (2) results (see below based on the 4-gram data)
+# > gr4 %>% filter(w4=='mendatang', w3 == 'yang')
+# # A tibble: 2 x 5
+# ngram                             w1     w2          w3    w4       
+# <chr>                             <chr>  <chr>       <chr> <chr>    
+# 1 Agenda_persidangan_yang_mendatang Agenda persidangan yang  mendatang
+# 2 di_waktu_yang_mendatang           di     waktu       yang  mendatang
+
+## checking 'X mendatang' in 2-gram data
+# x_mendatang <- gr2 %>% filter(w2 == "mendatang")
+# x_mendatang %>% write_tsv("data/x_mendatang.txt")
+x_mendatang <- read_tsv("data/x_mendatang.txt")
+x_mendatang %>% count(ngram, sort = TRUE)
+# lower-case the first word
+x_mendatang %>% 
+  mutate(across(where(is_character), str_to_lower)) %>% 
+  count(ngram, sort = TRUE)
+
+## checking 'X X mendatang' in 3-gram data
+# x_x_mendatang <- gr3 %>% filter(w3 == "mendatang")
+# x_x_mendatang %>% write_tsv("data/x_x_mendatang.txt")
+x_x_mendatang <- read_tsv("data/x_x_mendatang.txt")
+x_x_mendatang %>% count(ngram, sort = TRUE)
+# lower-case the first word
+x_x_mendatang %>% 
+  mutate(across(where(is_character), str_to_lower)) %>% 
+  count(ngram, sort = TRUE)
+
+## checking 'X X X mendatang' in 4-gram data
+# x_x_x_mendatang <- gr4 %>% filter(w4 == "mendatang")
+# x_x_x_mendatang %>% write_tsv("data/x_x_x_mendatang.txt")
+x_x_x_mendatang <- read_tsv("data/x_x_x_mendatang.txt")
+x_x_x_mendatang %>% count(ngram, sort = TRUE)
+# lower-case the first word
+x_x_x_mendatang %>% 
+  mutate(across(where(is_character), str_to_lower)) %>% 
+  count(ngram, sort = TRUE)
+
+## checking 'datang'
+# datang_gr2 <- gr2 %>% filter(if_any(starts_with("w"), ~str_detect(., "^datang$")))
+# datang_gr2 %>% write_tsv("data/datang_2grams.txt")
+# datang_gr3 <- gr3 %>% filter(if_any(starts_with("w"), ~str_detect(., "^datang$")))
+# datang_gr3 %>% write_tsv("data/datang_3grams.txt")
+# datang_gr4 <- gr4 %>% filter(if_any(starts_with("w"), ~str_detect(., "^datang$")))
+# datang_gr4 %>% write_tsv("data/datang_4grams.txt")
+datang_gr2 <- read_tsv("data/datang_2grams.txt") %>% 
+  mutate(across(where(is_character), str_to_lower))
+datang_gr3 <- read_tsv("data/datang_3grams.txt") %>% 
+  mutate(across(where(is_character), str_to_lower))
+datang_gr4 <- read_tsv("data/datang_4grams.txt") %>% 
+  mutate(across(where(is_character), str_to_lower))
+
+## count and filter the ngram where "datang" is the last word in the sequence
+datang_gr2 %>% count(ngram, sort = TRUE) %>% filter(str_detect(ngram, "_datang$")) # not too revealing
+datang_gr3 %>% count(ngram, sort = TRUE) %>% filter(str_detect(ngram, "_datang$")) # "yang akan datang" as the top pattern, but still can be anything.
+datang_gr4 %>% count(ngram, sort = TRUE) %>% filter(str_detect(ngram, "_datang$")) # better!
+
+
+
 
 
 
